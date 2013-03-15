svn2github/wp-svn-2-git

commit ae7ffec4444fe65452858a13e749701c3669893a

Twenty Twelve: enable live preview for Header Text Color in Theme Customizer. Change also removes an `!important` declaration (yay) in Custom Header CSS rules.
Props SriniG, fixes #23600.

git-svn-id: http://core.svn.wordpress.org/trunk@23572 1a063a9b-81f0-0310-95a4-ce76da25c4cd

wp-content/themes/twentytwelve/inc/custom-header.php
@@ -77,9 +77,9 @@ function twentytwelve_header_style() {
 		// If the user has set a custom color for the text, use that.
 		else :
 	?>
-		.site-title a,
-		.site-description {
-			color: #<?php echo $text_color; ?> !important;
+		.site-header h1 a,
+		.site-header h2 {
+			color: #<?php echo $text_color; ?>;
 		}
 	<?php endif; ?>
 	</style>

wp-content/themes/twentytwelve/js/theme-customizer.js
@@ -17,6 +17,12 @@
 			$( '.site-description' ).text( to );
 		} );
 	} );
+	// Header text color
+	wp.customize( 'header_textcolor', function( value ) {
+		value.bind( function( to ) {
+			$( '.site-title a, .site-description' ).css( 'color', to );
+		} );
+	} );
 
 	// Hook into background color/image change and adjust body class value as needed.
 	wp.customize( 'background_color', function( value ) {

wp-content/themes/twentytwelve/functions.php
@@ -437,8 +437,9 @@ add_action( 'template_redirect', 'twentytwelve_content_width' );
  * @return void
  */
 function twentytwelve_customize_register( $wp_customize ) {
-	$wp_customize->get_setting( 'blogname' )->transport = 'postMessage';
-	$wp_customize->get_setting( 'blogdescription' )->transport = 'postMessage';
+	$wp_customize->get_setting( 'blogname' )->transport         = 'postMessage';
+	$wp_customize->get_setting( 'blogdescription' )->transport  = 'postMessage';
+	$wp_customize->get_setting( 'header_textcolor' )->transport = 'postMessage';
 }
 add_action( 'customize_register', 'twentytwelve_customize_register' );
 

commit 11de178c8ba71afe0f006e11ac5384a32751603f

Twenty Twelve: fix regression introduced in r23572, allow hide/show of Header Text in Customizer. Fixes #23600.
Also remove another `!important` -- and there was much rejoicing.

Props SergeyBiryukov and obenland.

git-svn-id: http://core.svn.wordpress.org/trunk@23711 1a063a9b-81f0-0310-95a4-ce76da25c4cd

wp-content/themes/twentytwelve/inc/custom-header.php
@@ -69,7 +69,7 @@ function twentytwelve_header_style() {
 	?>
 		.site-title,
 		.site-description {
-			position: absolute !important;
+			position: absolute;
 			clip: rect(1px 1px 1px 1px); /* IE7 */
 			clip: rect(1px, 1px, 1px, 1px);
 		}

wp-content/themes/twentytwelve/js/theme-customizer.js
@@ -17,10 +17,22 @@
 			$( '.site-description' ).text( to );
 		} );
 	} );
+
 	// Header text color
 	wp.customize( 'header_textcolor', function( value ) {
 		value.bind( function( to ) {
-			$( '.site-title a, .site-description' ).css( 'color', to );
+			if ( 'blank' === to ) {
+				$( '.site-title, .site-title a, .site-description' ).css( {
+					'clip': 'rect(1px, 1px, 1px, 1px)',
+					'position': 'absolute'
+				} );
+			} else {
+				$( '.site-title, .site-title a, .site-description' ).css( {
+					'clip': 'auto',
+					'color': to,
+					'position': 'relative'
+				} );
+			}
 		} );
 	} );
 